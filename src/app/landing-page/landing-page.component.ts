6fc9a13f40018834c7379c3d65f4dc93c56ed839
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2025-03-27 10:43:40 +0530

    86cyct7w9 Basic html & css for header component.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
