23b1c1545886b236247011029faf997659bbfb66
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2025-03-29 12:35:47 +0530

    86cyct7w9 Add navbar drawer

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  isDrawerHidden: boolean = true;
 
+  toggleNavbarDrawer(event: Event) {
+    this.isDrawerHidden = !this.isDrawerHidden;
+  }
 }
