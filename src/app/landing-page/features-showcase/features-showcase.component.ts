a0cc84ae6b6eb3fa1b52377a02a3b7303500fce5
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2025-04-01 23:51:20 +0530

    86cyf584t Initial implementation.

---
+++ src/app/landing-page/features-showcase/features-showcase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features-showcase',
+  standalone: true,
+  imports: [],
+  templateUrl: './features-showcase.component.html',
+  styleUrl: './features-showcase.component.css'
+})
+export class FeaturesShowcaseComponent {
+
+}
