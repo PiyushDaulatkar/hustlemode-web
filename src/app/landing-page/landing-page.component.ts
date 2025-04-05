9d4ae5eae8f599482978cf035bfcf53b2776222e
PiyushDaulatkar <piyushdaulatkar03@gmail.com>
2025-04-05 20:09:25 +0530

    86cyct842 Add footer component.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { HeroBannerComponent } from './hero-banner/hero-banner.component';
 import { TestimonialsComponent } from "./testimonials/testimonials.component";
 import { FeaturesShowcaseComponent } from "./features-showcase/features-showcase.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent, HeroBannerComponent, TestimonialsComponent, FeaturesShowcaseComponent],
+  imports: [HeaderComponent, HeroBannerComponent, TestimonialsComponent, FeaturesShowcaseComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
 })
